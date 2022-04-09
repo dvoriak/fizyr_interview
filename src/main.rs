b40a3ac2a70c443ffe91d4761942fe3eb942272b
Vasyl <dvoryak.va@gmail.com>
2022-04-09 20:26:50 +0200

    Changes to dev to create PR

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused)]
 use clap::Parser;
+use reqwest;
 
 /// Read and create comments for GitHub PR
 #[derive(Parser)]
@@ -8,6 +9,16 @@ struct Cli {
     pr_id: String,
 }
 
+fn get_pr_comments(pr_id: String, client: reqwest::Client) {
+    let get_url = "https://api.github.com/repos/dvoriak/fizyr_interview/pulls/comments";
+    let res = client.get(get_url).await?.text().await?;
+
+    return res
+}
+
 fn main() {
     let args = Cli::parse();
+    let client = reqwest::Client::new();
+
+    println!("body = {:?}", get_pr_comments(args.pr_id, client))
 }
